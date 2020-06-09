e0822386328722f1f45139e81cd245b46bb2ff3a
Albert Gazendam <albert.gazendam@gmail.com>
2020-06-09 18:32:01 +0200

    4th commit

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir $HOME/.secrets
+mkdir ./.secrets
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DRIVE_SECRET_PASSPHRASE" \
---output $HOME/.secrets/my-project-92901-skicka-8a920f029b4b.json my-project-92901-skicka-8a920f029b4b.json.gpg
+--output ./.secrets/my-project-92901-skicka-8a920f029b4b.json my-project-92901-skicka-8a920f029b4b.json.gpg
