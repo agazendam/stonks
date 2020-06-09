f4cfc98971dcfb0480a14a9dbb50d4804be5f431
Albert Gazendam <albert.gazendam@gmail.com>
2020-06-09 21:06:41 +0200

    13th commit

--- .github/actions/process-action/entrypoint.sh
+++ .github/actions/process-action/entrypoint.sh
@@ -3,13 +3,13 @@
 apt-get update
 
 apt-get install -y --no-install-recommends r-cran-rcpp r-cran-zoo libquantlib0-dev ccache
-        && install.r tinytest
-        && mkdir ~/.R
-        && echo _R_CHECK_FORCE_SUGGESTS_=FALSE > ~/.R/check.Renviron
-        && echo CC=ccache gcc > ~/.R/Makevars
-        && echo CXX=ccache g++ >> ~/.R/Makevars
-        && echo CXX11=ccache g++ >> ~/.R/Makevars
-        && echo CXX14=ccache g++ >> ~/.R/Makevars
+install.r tinytest
+mkdir ~/.R
+echo _R_CHECK_FORCE_SUGGESTS_=FALSE > ~/.R/check.Renviron
+echo CC=ccache gcc > ~/.R/Makevars
+echo CXX=ccache g++ >> ~/.R/Makevars
+echo CXX11=ccache g++ >> ~/.R/Makevars
+echo CXX14=ccache g++ >> ~/.R/Makevars
 
 apt-get install -y -qq r-cran-rquantlib
 
